57ee95b129dcc5adfac52f7e04db0cc392f03061
scott2008 <ckim48@u.rochester.edu>
2021-12-12 10:30:21 +0900

    Upload

---
+++ 121221.py
@@ -0,0 +1,14 @@
+
+# Take 10 integers from keyboard using loop and print their average value on the screen.
+
+i = 10
+sum = 0
+while i>0:
+    num = int(input("Enter a Number: "))
+    sum = sum + num
+    i = i-1
+
+#print average
+    
+
+
